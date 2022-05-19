0959053d9837b1bd1fc102fb6e4bd3c18ef4d846
Cristovao Vilela <cvilela@lxplus7100.cern.ch>
2022-05-20 00:32:09 +0200

    Fedra build

--- defaults-release.sh
+++ defaults-release.sh
@@ -324,6 +324,14 @@ overrides:
       ls $GOOGLETEST_ROOT/lib/libgtest.a > /dev/null && \
       ls $GOOGLETEST_ROOT/lib/libgtest_main.a > /dev/null && \
       true
+  fedra:
+    tag: master
+    prefer_system_check: |
+      ls $FEDRA_ROOT/bin > /dev/null && \
+      ls $FEDRA_ROOT/lib > /dev/null && \
+      ls $FEDRA_ROOT/include > /dev/null && \
+      ls $FEDRA_ROOT/python > /dev/null && \
+      true
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
